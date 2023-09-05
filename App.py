99447cd19f9bf915c816b592a9e7e09f744c13d5
vikash-ai <65506030+vikash-ai@users.noreply.github.com>
2023-09-04 23:26:22 -0500

    Update App.py

--- App.py
+++ App.py
@@ -80,7 +80,7 @@ Model predicts the probability of a customer loan application Approval using his
     st.subheader('User Input features')
     st.dataframe(input_df, hide_index=True)
     #st.write(input_df.reset_index(drop=True))
-    with open("C:/HMDA/Model/approval_pipeline_tuned.pkl", 'rb') as pfile:  
+    with open("./approval_pipeline_tuned.pkl", 'rb') as pfile:  
                 load_clf=pickle.load(pfile)
     NUMERICAL_VARIABLES = ['loan_amount', 'income','loan_term','property_value','applicant_credit_score_type']
     CATEGORICAL_VARIABLES = ['debt_to_income_ratio', 'loan_purpose']
@@ -115,7 +115,7 @@ def tab2_content():
     #@st.cache_data
     def load_model(pkl):
         return pickle.load(open(pkl, "rb"))
-    model = load_model("C:/HMDA/Model/approval_pipeline_tuned.pkl")
+    model = load_model("./approval_pipeline_tuned.pkl")
     # Extract the final estimator from the pipeline
     final_estimator = model.named_steps['RF_tuned']
    # Apply the scaler to X_test
