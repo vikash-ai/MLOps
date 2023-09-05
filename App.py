5e338be84750856086fb3b46a93bf9449fefd007
vikash-ai <65506030+vikash-ai@users.noreply.github.com>
2023-09-04 22:23:52 -0500

    Add files via upload

--- App.py
+++ App.py
@@ -17,61 +17,74 @@ import joblib
 import pickle
 import io
 import shap
-import altair as alt
+# import altair as alt
 
+# Define a custom SessionState class
+class SessionState:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+# Create a session state to store tab-specific variables
+session_state = SessionState(
+    uploaded_file_tab1=None,
+    uploaded_file_tab2=None,
+    uploaded_file_tab3=None
+)
 # Define functions for each tab's content
 def tab1_content():
-    # Sidebar content for Tab 1
-    #st.sidebar.write("Enter Your Parameter")
-    # Main content for Tab 1
+    # session_state.uploaded_file_tab2 = None
+    # session_state.uploaded_file_tab3 = None
     st.write("""
 
-# Loan Application Prediction Model
+# Model Inference by Loan Id
 
-Model predicts the probability of a customer loan application Approval using Bank past Loan application data
+Model predicts the probability of a customer loan application Approval using historical application data
 
     """)
-    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-    st.sidebar.write("OR Input Your Features below")
-    if uploaded_file is not None:
+    #uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+    st.sidebar.write("Input your loan application below")
+    # if uploaded_file is not None:
+    #     session_state.uploaded_file_tab1 = uploaded_file
+    #     input_df1 = pd.read_csv(uploaded_file)
+    #     input_df = input_df1[['applicant_credit_score_type','debt_to_income_ratio','loan_amount','property_value','income','loan_purpose', 'loan_term']]
+    #     input_df['loan_purpose'] = input_df['loan_purpose'].astype('O')
+    # else:
+    input_df = pd.DataFrame()
+    # def user_input():
 
-        input_df1 = pd.read_csv(uploaded_file)
-        input_df = input_df1[['applicant_credit_score_type','debt_to_income_ratio','loan_amount','property_value','income','loan_purpose', 'loan_term']]
-        input_df['loan_purpose'] = input_df['loan_purpose'].astype('O')
-    else:
+    loan_amount = st.sidebar.slider('Loan Amount', 5000.0,500000.0, 1000.0)
 
-        def user_input():
+    loan_purpose = st.sidebar.selectbox('Loan Purpose',('1','2','4','31','32'))
 
-            loan_amount = st.sidebar.slider('Loan Amount', 5000.0,500000.0, 1000.0)
+    loan_term = st.sidebar.slider('Loan Term', 12,360, 12)
+    property_value = st.sidebar.slider('Property value', 5000.0,50000000.0, 1000.0)
+    income = st.sidebar.slider('Income', 0.0,20000.0, 100.0)
+    debt_to_income_ratio = st.sidebar.selectbox('DTI',('<20%','20%-<30%','30%-<36%','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50%-60%','>60%'))
+    applicant_credit_score_type = st.sidebar.selectbox('Credit Score',('1', '2','3','8','9'))
+    
+    data = {'Application No': 1001,
+            'loan_amount':[loan_amount],
+            'loan_purpose':[loan_purpose],
+            'loan_term':[loan_term],
+            'property_value':[property_value],
+            'income':[income],
+            'debt_to_income_ratio':[debt_to_income_ratio],
+            'applicant_credit_score_type':[applicant_credit_score_type],
+            }
+    features = pd.DataFrame(data)
 
-            loan_purpose = st.sidebar.selectbox('Loan Purpose',('1','2','4','31','32'))
-
-            loan_term = st.sidebar.slider('Loan Term', 12,360, 12)
-            property_value = st.sidebar.slider('Property value', 5000.0,50000000.0, 1000.0)
-            income = st.sidebar.slider('Income', 0.0,20000.0, 100.0)
-            debt_to_income_ratio = st.sidebar.selectbox('DTI',('<20%','20%-<30%','30%-<36%','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50%-60%','>60%'))
-            applicant_credit_score_type = st.sidebar.selectbox('Credit Score',('1', '2','3','8','9'))
-            
-            data = {'loan_amount':[loan_amount],
-                    'loan_purpose':[loan_purpose],
-                    'loan_term':[loan_term],
-                    'property_value':[property_value],
-                    'income':[income],
-                    'debt_to_income_ratio':[debt_to_income_ratio],
-                    'applicant_credit_score_type':[applicant_credit_score_type],
-                    }
-            features = pd.DataFrame(data)
-
-            return features
-
-        input_df = user_input()
+    # return features
+    # input_df = pd.DataFrame()
+    input_df = features
 
     st.subheader('User Input features')
-    st.write(input_df)
-    with open("./approval_pipeline_tuned.pkl", 'rb') as pfile:  
+    st.dataframe(input_df, hide_index=True)
+    #st.write(input_df.reset_index(drop=True))
+    with open("C:/HMDA/Model/approval_pipeline_tuned.pkl", 'rb') as pfile:  
                 load_clf=pickle.load(pfile)
     NUMERICAL_VARIABLES = ['loan_amount', 'income','loan_term','property_value','applicant_credit_score_type']
     CATEGORICAL_VARIABLES = ['debt_to_income_ratio', 'loan_purpose']
+    input_df = input_df.drop('Application No', axis=1)
     prediction = load_clf.predict(input_df)
     # st.write(input_df)
 
@@ -85,23 +98,24 @@ Model predicts the probability of a customer loan application Approval using Ban
     st.write(prediction_proba)
 
 def tab2_content():
-    # Sidebar content for Tab 2
-    #st.sidebar.write("This is the sidebar for Tab 2")
-    # Main content for Tab 2
+    session_state.uploaded_file_tab1 = None
+    session_state.uploaded_file_tab3 = None
     st.write("Show Me Model Behaviour")
     # load pickle file
     # write as function so we can cache it
-    @st.cache_data
+    #@st.cache_data
     def load_model(pkl):
         return pickle.load(open(pkl, "rb"))
-    model = load_model("./approval_pipeline_tuned.pkl")
+    model = load_model("C:/HMDA/Model/approval_pipeline_tuned.pkl")
     # Extract the final estimator from the pipeline
     final_estimator = model.named_steps['RF_tuned']
    # Apply the scaler to X_test
     encoder = model.named_steps['categorical_encoder']
-    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-    if uploaded_file is not None:
-        input_df1 = pd.read_csv(uploaded_file)
+    uploaded_file1 = st.file_uploader("Upload your input CSV file", type=["csv"])
+    if uploaded_file1 is not None:
+        session_state.uploaded_file_tab2 = uploaded_file1
+        session_state.uploaded_file_tab3 = None
+        input_df1 = pd.read_csv(uploaded_file1)
         X1 = input_df1[['applicant_credit_score_type','debt_to_income_ratio','loan_amount','property_value','income','loan_purpose', 'loan_term']]
         X1['loan_purpose'] = X1['loan_purpose'].astype('O')
         #take smaller sample to run the shap value analysis
@@ -110,14 +124,7 @@ def tab2_content():
         X = X2.reset_index(drop=True) 
     else:
         st.stop()
-        #X = pd.DataFrame()
-        # input_df1 = pd.read_csv("C:/HMDA/Input/df1.csv")
-        # X1 = input_df1[['applicant_credit_score_type','debt_to_income_ratio','loan_amount','property_value','income','loan_purpose', 'loan_term']]
-        # X1['loan_purpose'] = X1['loan_purpose'].astype('O')
-        # #take smaller sample to run the shap value analysis
-        # X2 = shap.sample(X1, 100)
-        # X = X2.reset_index(drop=True)
-        # nobs = 20  # set a default value here
+        
     with st.expander("Model Features and Data summary"):
         if not X.empty:
             nobs = st.slider(
@@ -130,25 +137,33 @@ def tab2_content():
     
         # Conditionally calculate summary statistics
         if st.checkbox("Display summary statistics for visible sample?"):
-            f"""Sample statistics based on {nobs} observations:"""
-            st.dataframe(X.head(nobs).describe())
+            f"""Sample statistics based on uploaded Data:"""
+            #f"""Sample statistics based on {nobs} observations:"""
+            st.dataframe(X1.describe())
+            #st.dataframe(X.head(nobs).describe())
         X_encoder = encoder.transform(X)
     #with st.echo():
+    explainer = shap.Explainer(final_estimator,X_encoder)
+    shap_values = explainer(X_encoder)   
+    "### Feature importance - All Batch"
     explainer = shap.TreeExplainer(final_estimator)
-    shap_values = explainer.shap_values(X_encoder)   
-    "### Feature importance - All"
-    st_shap(shap.force_plot(explainer.expected_value[0], shap_values[0], X_encoder), height=350)
+    shap_values = explainer.shap_values(X_encoder)
     st_shap(shap.summary_plot(shap_values, X_encoder))
-    #st_shap(shap.force_plot(explainer.expected_value, shap_values, X_encoder), height=350)
+    st_shap(shap.summary_plot(shap_values[1], X_encoder))
     feature = st.selectbox("Choose Application Number", X.index.values)
     f"### Application No. {feature}: Explanation For Loan Approval"
     st_shap(shap.force_plot(explainer.expected_value[0], shap_values[0][feature], X_encoder.iloc[feature]), height=350)
 
 def tab3_content():
     # Sidebar content for Tab 3
-    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-    if uploaded_file is not None:
-        input_df1 = pd.read_csv(uploaded_file)
+    session_state.uploaded_file_tab1 = None
+    session_state.uploaded_file_tab2 = None
+    uploaded_file2 = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"], key="uploaded_file2")
+    if uploaded_file2 is not None:
+        session_state.uploaded_file_tab3 = uploaded_file2
+        session_state.uploaded_file_tab2 = None
+        #st.session_state["uploaded_file2"]
+        input_df1 = pd.read_csv(st.session_state['uploaded_file2'])
         X1 = input_df1[['derived_msa-md','derived_race','derived_sex','applicant_age_above_62','loan_amount','interest_rate','income','action_taken']]
         action_to_filter = [1,3]
         X1 = X1[X1['action_taken'].isin(action_to_filter)]
@@ -160,7 +175,8 @@ def tab3_content():
     else:
         st.stop()
     # Main content for Tab 3
-    st.write(X1.head(3))
+    #st.write(X1.sample(3))
+    st.dataframe(X1.sample(5), hide_index=True)
     fig, axs = plt.subplots(1, 3, figsize=(15, 10))
     columns = ['derived_sex', 'derived_race', 'applicant_age_above_62']
     titles = ['Gender Distribution', 'Race Distribution', 'Age Above 62']
@@ -182,53 +198,112 @@ def tab3_content():
         st.write("Select Option to view the distribution plots")
     
     #APR distribution  
-    st.set_option('deprecation.showPyplotGlobalUse', False)
+    #st.set_option('deprecation.showPyplotGlobalUse', False)
     show_APR = st.sidebar.checkbox("Show APR Distribution")
     if show_APR:
         st.header("Loan Density By Prohibited Basis Factor")
-        #sns.set(style="whitegrid")
-        #plt.figure(figsize=(6, 4))
+        fig, axes = plt.subplots(1, 2, figsize=(20, 10))
         df_r = X1[(X1.derived_race=='White')|(X1.derived_race=='Black or African American') ]
-        sns.displot(data=df_r,x='interest_rate', hue='derived_race',kind='kde',fill=True,palette='tab10')
-        plt.title("Loan Density Plot For Black or African American")
-        plt.xlabel("APR")
-        plt.grid(False)
-        st.pyplot()
+        #sns.displot(data=df_r,x='interest_rate', hue='derived_race',kind='kde',fill=True,palette='tab10', ax=ax1)
+        sns.kdeplot(data=df_r, x='interest_rate', hue='derived_race', fill=True, palette='tab10', ax=axes[0])
+        axes[0].set_title("Loan Density Plot For Black or African American")
+        axes[0].set_xlabel("APR")
+        axes[0].grid(False)
+        DIR_ratio = 0.12  # Replace this with your actual DIR ratio
+        bar_labels = ['White', 'Black or African American']  # Labels for the bars
+        bar_heights = [1.0, DIR_ratio]  # Heights of the bars, where 1.0 represents no disparate impact
+
+        axes[1].bar(bar_labels, bar_heights, color=['blue', 'red'])
+        axes[1].set_title("Disparate Impact Ratio")
+        axes[1].set_ylabel("DIR Ratio")
+        axes[1].grid(False)
+        st.pyplot(fig)
         
+        fig1, axes = plt.subplots(1, 2, figsize=(20, 10))
         df_r = X1[(X1.derived_race=='White')|(X1.derived_race=='Asian') ]
-        sns.displot(data=df_r,x='interest_rate', hue='derived_race',kind='kde',fill=True,palette='tab10')
-        plt.title("Loan Density Plot For Asian")
-        plt.xlabel("APR")
-        plt.grid(False)
-        st.pyplot()
+        #sns.displot(data=df_r,x='interest_rate', hue='derived_race',kind='kde',fill=True,palette='tab10')
+        sns.kdeplot(data=df_r, x='interest_rate', hue='derived_race', fill=True, palette='tab10', ax=axes[0])
+        axes[0].set_title("Loan Density Plot For Asian")
+        axes[0].set_xlabel("APR")
+        axes[0].grid(False)
+        DIR_ratio = 0.13  # Replace this with your actual DIR ratio
+        bar_labels = ['White', 'Asian']  # Labels for the bars
+        bar_heights = [1.0, DIR_ratio]  # Heights of the bars, where 1.0 represents no disparate impact
+
+        axes[1].bar(bar_labels, bar_heights, color=['blue', 'red'])
+        axes[1].set_title("Disparate Impact Ratio")
+        axes[1].set_ylabel("DIR Ratio")
+        axes[1].grid(False)
+        st.pyplot(fig1)
         
+        fig2, axes = plt.subplots(1, 2, figsize=(20, 10))
         df_r = X1[(X1.derived_race=='White')|(X1.derived_race=='American Indian or Alaska Native') ]
-        sns.displot(data=df_r,x='interest_rate', hue='derived_race',kind='kde',fill=True,palette='tab10')
-        plt.title("Loan Density Plot For American Indian or Alaska Native")
-        plt.xlabel("APR")
-        plt.grid(False)
-        st.pyplot()
+        #sns.displot(data=df_r,x='interest_rate', hue='derived_race',kind='kde',fill=True,palette='tab10')
+        sns.kdeplot(data=df_r, x='interest_rate', hue='derived_race', fill=True, palette='tab10', ax=axes[0])
+        axes[0].set_title("Loan Density Plot For American Indian or Alaska Native")
+        axes[0].set_xlabel("APR")
+        axes[0].grid(False)
+        DIR_ratio = 0.02  # Replace this with your actual DIR ratio
+        bar_labels = ['White', 'American Indian or Alaska Native']  # Labels for the bars
+        bar_heights = [1.0, DIR_ratio]  # Heights of the bars, where 1.0 represents no disparate impact
+
+        axes[1].bar(bar_labels, bar_heights, color=['blue', 'red'])
+        axes[1].set_title("Disparate Impact Ratio")
+        axes[1].set_ylabel("DIR Ratio")
+        axes[1].grid(False)
+        st.pyplot(fig2)
         
+        fig3, axes = plt.subplots(1, 2, figsize=(20, 10))
         df_r = X1[(X1.derived_race=='White')|(X1.derived_race=='Native Hawaiian or Other Pacific Islander') ]
-        sns.displot(data=df_r,x='interest_rate', hue='derived_race',kind='kde',fill=True,palette='tab10')
-        plt.title("Loan Density Plot For  Other Pacific Islander")
-        plt.xlabel("APR")
-        plt.grid(False)
-        st.pyplot()
+        #sns.displot(data=df_r,x='interest_rate', hue='derived_race',kind='kde',fill=True,palette='tab10')
+        sns.kdeplot(data=df_r, x='interest_rate', hue='derived_race', fill=True, palette='tab10', ax=axes[0])
+        axes[0].set_title("Loan Density Plot For  Other Pacific Islander")
+        axes[0].set_xlabel("APR")
+        axes[0].grid(False)
+        DIR_ratio = 0.01  # Replace this with your actual DIR ratio
+        bar_labels = ['White', 'Native Hawaiian or Other Pacific Islander']  # Labels for the bars
+        bar_heights = [1.0, DIR_ratio]  # Heights of the bars, where 1.0 represents no disparate impact
+
+        axes[1].bar(bar_labels, bar_heights, color=['blue', 'red'])
+        axes[1].set_title("Disparate Impact Ratio")
+        axes[1].set_ylabel("DIR Ratio")
+        axes[1].grid(False)
+        st.pyplot(fig3)
         
+        fig4, axes = plt.subplots(1, 2, figsize=(20, 10))
         df_r = X1[(X1.derived_sex=='Male')|(X1.derived_sex=='Female') ]
-        sns.displot(data=df_r,x='interest_rate', hue='derived_sex',kind='kde',fill=True,palette='tab10')
-        plt.title("Loan Density Plot For Gender")
-        plt.xlabel("APR")
-        plt.grid(False)
-        st.pyplot()
+        #sns.displot(data=df_r,x='interest_rate', hue='derived_sex',kind='kde',fill=True,palette='tab10')
+        sns.kdeplot(data=df_r, x='interest_rate', hue='derived_sex', fill=True, palette='tab10', ax=axes[0])
+        axes[0].set_title("Loan Density Plot For Gender")
+        axes[0].set_xlabel("APR")
+        axes[0].grid(False)
+        DIR_ratio = 0.30  # Replace this with your actual DIR ratio
+        bar_labels = ['Male', 'Female']  # Labels for the bars
+        bar_heights = [1.0, DIR_ratio]  # Heights of the bars, where 1.0 represents no disparate impact
+
+        axes[1].bar(bar_labels, bar_heights, color=['blue', 'red'])
+        axes[1].set_title("Disparate Impact Ratio")
+        axes[1].set_ylabel("DIR Ratio")
+        axes[1].grid(False)
+        st.pyplot(fig4)
         
+        fig5, axes = plt.subplots(1, 2, figsize=(20, 10))
         df_r = X1[(X1.applicant_age_above_62=='Yes')|(X1.applicant_age_above_62=='No') ]
-        sns.displot(data=df_r,x='interest_rate', hue='applicant_age_above_62',kind='kde',fill=True,palette='tab10')
-        plt.title("Loan Density Plot For Age Above 62")
-        plt.xlabel("APR")
-        plt.grid(False)
-        st.pyplot()
+        #sns.displot(data=df_r,x='interest_rate', hue='applicant_age_above_62',kind='kde',fill=True,palette='tab10')
+        sns.kdeplot(data=df_r, x='interest_rate', hue='applicant_age_above_62', fill=True, palette='tab10', ax=axes[0])
+        axes[0].set_title("Loan Density Plot For Age Above 62")
+        axes[0].set_xlabel("APR")
+        axes[0].grid(False)
+        DIR_ratio = 0.30  # Replace this with your actual DIR ratio
+        bar_labels = ['No', 'Yes']  # Labels for the bars
+        bar_heights = [1.0, DIR_ratio]  # Heights of the bars, where 1.0 represents no disparate impact
+
+        axes[1].bar(bar_labels, bar_heights, color=['blue', 'red'])
+        axes[1].set_title("Disparate Impact Ratio")
+        axes[1].set_ylabel("DIR Ratio")
+        axes[1].grid(False)
+        
+        st.pyplot(fig5)
         # Filter the data by each race class and create displots for APR within each class
         # unique_races = X1['derived_race'].unique()
         # for race in unique_races:
@@ -255,10 +330,10 @@ tabs = ["ML Model", "Model Explability", "Fair Lending Diaganosis"]
 selected_tab = st.radio("Select a tab:", tabs, format_func=lambda x: x)
 
 # Display content based on selected tab
+
 if selected_tab == "ML Model":
     tab1_content()
 elif selected_tab == "Model Explability":
     tab2_content()
 else:
     tab3_content()
-
